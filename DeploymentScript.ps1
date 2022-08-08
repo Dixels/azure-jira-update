492217fd4018567aad1aaaf7cc952908fbedd2da
Dixels <106463890+Dixels@users.noreply.github.com>
2022-08-08 23:20:54 +0300

    Update DeploymentScript.ps1

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -1,3 +1,4 @@
 Import-Module (Join-Path "." "EP.PowerShell.JiraDeployInfo")
 $DebugPreference="Continue"
+$VerbosePreference = "Continue"
 Update-AzureDeploymentInformation -State $Env:JIRA_STATE -EnvironmentType $Env:JIRA_ENVIRONMENT -JiraDomain $Env:JIRA_DOMAIN
