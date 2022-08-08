7bbfb84eaa5278fe80bbeee67dcf34a813b72b25
Dixels <106463890+Dixels@users.noreply.github.com>
2022-08-08 23:11:20 +0300

    Update DeploymentScript.ps1

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -1,3 +1,3 @@
 Import-Module (Join-Path "." "EP.PowerShell.JiraDeployInfo")
 $DebugPreference="Continue"
-Update-AzureDeploymentInformation -State $Env:JIRA_STATE -EnvironmentType $Env:JIRA_ENVIRONMENT -JiraDomain $Env:JIRA_DOMAIN -BitBucketUsername $Env:BITBUCKET_USERNAME -BitBucketPassword $Env:BITBUCKET_PASSWORD
+Update-AzureDeploymentInformation -State $Env:JIRA_STATE -EnvironmentType $Env:JIRA_ENVIRONMENT -JiraDomain $Env:JIRA_DOMAIN
