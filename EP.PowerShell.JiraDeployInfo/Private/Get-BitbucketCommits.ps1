09ed337b0c317a4d9221a61cf50a6e4fadf25b45
Dixels <106463890+Dixels@users.noreply.github.com>
2022-08-08 21:20:50 +0300

    Create Get-BitbucketCommits.ps1

---
+++ EP.PowerShell.JiraDeployInfo/Private/Get-BitbucketCommits.ps1
@@ -0,0 +1,22 @@
+function Get-BitbucketCommits {
+    param (
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+        [string] $Username,
+        
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+        [string] $Password,
+
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+        [string] $BitBucketCommitsUrl 
+    )
+    
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username,$Password)))
+    $response = Invoke-RestMethod -Uri $BitBucketCommitsUrl -Headers @{Authorization = "Basic $base64AuthInfo" } -Method Get
+    Write-Verbose("Azure change response:" + ($response | ConvertTo-Json -Depth 100))
+
+    $build_changes = $response.values | ForEach-Object { $_.message }
+
+    Write-Verbose("[Build Changes count] " + $build_changes.Count)
+    Write-Verbose("[Build Changes] " + ($build_changes | ConvertTo-Json -Depth 100))
+    $build_changes
+}
