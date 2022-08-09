e7aa06fff589d7e5df376398a9e3d120a4f09f49
Dixels <106463890+Dixels@users.noreply.github.com>
2022-08-09 14:25:13 +0300

    Update Get-JiraIDsFromBitbucketCommits.ps1

--- EP.PowerShell.JiraDeployInfo/Private/Get-JiraIDsFromBitbucketCommits.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Get-JiraIDsFromBitbucketCommits.ps1
@@ -1,6 +1,12 @@
 function Get-JiraIDsFromBitbucketCommits {
     param (
         [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+        [string] $SystemAccessToken,
+        
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+        [string] $AzureLastSuccessfulBuildUrl 
+        
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
         [string] $Username,
         
         [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
@@ -11,7 +17,8 @@ function Get-JiraIDsFromBitbucketCommits {
     )
 
     $jiraIDs = @()
-    Get-BitbucketCommits -Username $Username -Password $Password -BitBucketCommitsUrl $BitBucketCommitsUrl | ForEach-Object {
+    $last_commit = Get-AzureDevopsLastSuccessfulBuildCommit -SystemAccessToken $SystemAccessToken -AzureLastSuccessfulBuildUrl $AzureLastSuccessfulBuildUrl
+    Get-BitbucketCommits -Username $Username -Password $Password -BitBucketCommitsUrl $BitBucketCommitsUrl -BitbucketLastBuildCommitHash $last_commit | ForEach-Object {
         Find-JiraIDs ($_) | ForEach-Object {
             $jiraIDs += $_.Value.ToUpper()
         }
