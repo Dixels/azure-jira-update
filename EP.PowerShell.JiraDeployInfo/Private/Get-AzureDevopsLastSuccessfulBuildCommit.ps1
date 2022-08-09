442ae01be6b8a687e97d92c0a26c59007687411f
Dixels <106463890+Dixels@users.noreply.github.com>
2022-08-09 15:06:36 +0300

    Update Get-AzureDevopsLastSuccessfulBuildCommit.ps1

--- EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsLastSuccessfulBuildCommit.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsLastSuccessfulBuildCommit.ps1
@@ -11,7 +11,7 @@ function Get-AzureDevopsLastSuccessfulBuildCommit {
     $response = Invoke-RestMethod -Uri $AzureLastSuccessfulBuildUrl -Headers @{Authorization = "Basic $token" } -Method Get
     Write-Verbose("Azure last successful build response:" + ($response | ConvertTo-Json -Depth 100))
     
-    $commit_id = $response.value[0].triggerInfo.'ci.sourceSha'
+    $commit_id = $response.value[0].sourceVersion
     Write-Verbose("[Azure last successful build commit] " + $commit_id)
     $commit_id
 }
