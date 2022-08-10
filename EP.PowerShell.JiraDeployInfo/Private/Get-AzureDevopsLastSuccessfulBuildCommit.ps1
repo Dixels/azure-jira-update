96efebcb106539c4beddb03de003c002faa871db
Dixels <106463890+Dixels@users.noreply.github.com>
2022-08-10 23:18:55 +0000

    Fix again

--- EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsLastSuccessfulBuildCommit.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Get-AzureDevopsLastSuccessfulBuildCommit.ps1
@@ -10,7 +10,7 @@ function Get-AzureDevopsLastSuccessfulBuildCommit {
     $token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($SystemAccessToken)"))
     $response = Invoke-RestMethod -Uri $AzureLastSuccessfulBuildUrl -Headers @{Authorization = "Basic $token" } -Method Get
     Write-Verbose("Azure last successful build response:" + ($response | ConvertTo-Json -Depth 100))
-    $commit_id = if ($response.value.count -gt 0) { $response.value[0].sourceVersion } else { '' }
+    $commit_id = if ($response.value.count -gt 0) { $response.value[0].sourceVersion } else { 'noHashFound' }
     Write-Verbose("[Azure last successful build commit] " + $commit_id)
     $commit_id
 }
